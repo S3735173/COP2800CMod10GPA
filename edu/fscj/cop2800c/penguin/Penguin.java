15b572c38a81ff81577a9e5ed2604068091c9ea5
S3735173 <s3735173@students.fscj.edu>
2026-03-28 12:10:06 -0400

    Inheritance and Polymorphism
    Completed class declaration, added private fields, added signature to public Bird and implemented public compare method.

--- edu/fscj/cop2800c/penguin/Penguin.java
+++ edu/fscj/cop2800c/penguin/Penguin.java
@@ -1,19 +1,22 @@
 // Penguin.java
-// 
-// 
-// Represents a Palmer Penguin
+// Kevin Gordie
+// 3/28/2026
+// Represents a penguin derived class
 
 package edu.fscj.cop2800c.penguin;
 
-// Penguin class extends the Bird base class
-public class Penguin {
-    // private fields
+public class Penguin extends Bird {
+    private String species;
+    private double flipperLength;
 
+    public Penguin(int sampleNum, String species,
+                   double culmenLength, double culmenDepth,
+                   double bodyMass, String sex, double flipperLength) {
+        super(sampleNum, culmenLength, culmenDepth, bodyMass, sex);
+        this.species = species;
+        this.flipperLength = flipperLength;
+    }
 
-    // Constructor
-
-
-    // Getter methods
     public String getSpecies() {
         return species;
     }
@@ -22,38 +25,10 @@ public class Penguin {
         return flipperLength;
     }
 
-    // Override Bird class's toString method
     @Override
     public String toString() {
-        return super.toString() + 
-               ", species='" + species + '\'' +
-               ", flipperLength=" + flipperLength;
-    }
-
-    // Override Bird's compareTo method to include Penguin field comparison
-    @Override
-    public int compareTo(Bird other) {
-        int result = super.compareTo(other);
-        if (result != 0) return result;
-
-        // Check if 'other' is a Penguin before 
-        // comparing flipper length and species
-        if (other instanceof Penguin) {
-            Penguin otherPenguin = (Penguin) other;
-    
-            // Compare flipper length
-            result = Double.compare(
-                this.flipperLength, otherPenguin.flipperLength);
-            if (result != 0) return result;
-    
-            // Compare species (handle potential null values)
-            if (this.species == null && otherPenguin.species == null) return 0;
-            if (this.species == null) return -1;
-            if (otherPenguin.species == null) return 1;
-    
-            return this.species.compareTo(otherPenguin.species);
-        }
-
-        return result;
+        return super.toString() +
+               ", species='" + species +
+               "', flipperLength=" + flipperLength;
     }
 }
